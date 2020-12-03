d1d62220682f505f0d29f43c372460e69fc0bc00
mo-jinbuu <mohit-patel7@hotmail.com>
2020-12-02 23:05:44 -0500

    add script and css styling

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ next.addEventListener('click', () => {
 
 
 prev.addEventListener('click', () => {
-  curentActive--;
+  currentActive--;
 
   if(currentActive < 1) {
     currentActive = 1;
@@ -26,11 +26,25 @@ prev.addEventListener('click', () => {
 })
 
 function update() {
+  console.log(currentActive)
   circles.forEach((circle, idx) => {
     if(idx < currentActive) {
       circle.classList.add('active')
     } else {
       circle.classList.remove('active')
+    }
+
+    const actives = document.querySelectorAll('.active')
+
+    progress.style.width = ((actives.length-1)/(circles.length-1)) * 100 + '%';
+
+    if(currentActive === 1) {
+      prev.disabled = true;
+    } else if (currentActive === circles.length) {
+      next.disabled =  true;
+    } else {
+      prev.disabled = false;
+      next.disabled = false;
     }
   })
 }
